36e7d7fdc8f3ff2d5bb20d4a823d9f49d3df138a
fukuyumi <kk.ma.yff@gmail.com>
2022-11-11 17:11:32 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    transient do
+      person {Gimei.name}
+    end
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = ('1a' + Faker::Internet.password(min_length: 6))
+    password {password}
     password_confirmation {password}
-    birthday              {Faker::Date.birthday(min_age: 5, max_age: 92)}
+    last_name {person.last.kanji}
+    first_name {person.first.kanji}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    birthday {Faker::Date.between(from: '1930-01-01', to: 5.years.ago)}
   end
 end
