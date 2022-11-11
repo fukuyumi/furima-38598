88b1c05d1c908e713b230fcec678cffd7256c3ac
fukuyumi <kk.ma.yff@gmail.com>
2022-11-11 18:46:34 +0900

    バリデーションの修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
       validates :nickname,
         presence: true
-      validates :encrypted_password,
-        format: { with:  /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/},
-        presence: true
+
+      VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+      validates :password, format: { with: VALID_PASSWORD_REGEX }
       validates :first_name,
         format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
         presence: true
