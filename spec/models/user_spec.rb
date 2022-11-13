4cdc7196d2bb140f83d80925bc52faba9396925f
fukuyumi <kk.ma.yff@gmail.com>
2022-11-14 07:46:43 +0900

    モデル単体テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -77,7 +77,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
-      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      it 'passwordとpassword_confirmationが一致していないと登録できない' do
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
