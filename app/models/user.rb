914ccaf4097b7e67378ee773e7fd47abaf94ed68
fukuyumi <kk.ma.yff@gmail.com>
2022-11-14 15:01:55 +0900

    テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates :nickname,
             presence: true
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
   validates :first_name,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/  },
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
             presence: true
   validates :last_name,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/  },
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
             presence: true
   validates :first_name_kana,
             format: { with: /\A[ァ-ヶー]+\z/ },
