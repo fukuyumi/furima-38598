0e9371b88c395e20f1719193883fe6cbeb83c76d
fukuyumi <kk.ma.yff@gmail.com>
2022-11-14 18:01:51 +0900

    モデル・アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :ship_charge
+  belongs_to :prefecture
+  belongs_to :ship_date
 
   belongs_to :user
   has_one_attached :image
