9d0f0cdbd7a9e9c9ba9274af8f8054893b9b61c3
fukuyumi <kk.ma.yff@gmail.com>
2022-11-14 21:41:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :item_status_id
+    validates :ship_charge_id
+    validates :prefecture_id
+    validates :ship_date_id
+  end
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :content, unless: :was_attached?
+    validates :category_id
+    validates :item_status_id
+    validates :ship_charge_id
+    validates :prefecture_id
+    validates :ship_date_id
+    validates :price, numericality: { in: 300..9999999 }
+  end
+
+  belongs_to :user
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_status
@@ -6,6 +29,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :ship_date
 
-  belongs_to :user
-  has_one_attached :image
+  def was_attached?
+    self.image.attached?
+  end
+
+
 end
