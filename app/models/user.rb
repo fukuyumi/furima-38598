16dec2377519ab21a120cc789631cb9749f52221
fukuyumi <kk.ma.yff@gmail.com>
2022-11-14 16:36:22 +0900

    モデル・テーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname,
             presence: true
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :first_name,
             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
             presence: true
