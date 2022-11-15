59f5716d64c9d14179007e5edba910669e3d3370
fukuyumi <kk.ma.yff@gmail.com>
2022-11-15 16:35:33 +0900

    出品商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :item_status_id, :ship_charge_id, :prefecture_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_description
-    validates :content, unless: :was_attached?
+    validates :image, unless: :was_attached?
     validates :category_id
     validates :item_status_id
     validates :ship_charge_id
     validates :prefecture_id
     validates :ship_date_id
-    validates :price, numericality: { in: 300..9999999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
   belongs_to :user
