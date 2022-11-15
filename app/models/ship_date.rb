20af1e80cdb1a18f32b1eb7d915164fdd62ecf6f
fukuyumi <kk.ma.yff@gmail.com>
2022-11-15 14:55:24 +0900

    出品ページの表示

--- app/models/ship_date.rb
+++ app/models/ship_date.rb
@@ -1,9 +1,9 @@
 class ShipDate < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '1~2日で発送' },
-    { id: 3, name: '2~3日で発送' },
-    { id: 4, name: '4~7日で発送' }
+    { id: 1, ship_date: '---' },
+    { id: 2, ship_date: '1~2日で発送' },
+    { id: 3, ship_date: '2~3日で発送' },
+    { id: 4, ship_date: '4~7日で発送' }
   ]
 
   include ActiveHash::Associations
