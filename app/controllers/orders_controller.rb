0d8cf488d1e55291d4f3a3e572299ab67cc4ac6b
fukuyumi <kk.ma.yff@gmail.com>
2022-11-19 17:10:56 +0900

    コントローラーへ記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   
   def index
   end
 
   def new
+    @order_address = OederAddress.new
   end
 
   def create
+    @order_address = OederAddress.new(order_params)
+    if @order_address.valid? 
+      @order_address.save
+      redirect_to root_path(@order_address)
+    else
+      render :new
   end
 
+private
+
+def order_params
+  params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :buliding, :phone_number).marge(user_id: current_user.id)
+end
 end
