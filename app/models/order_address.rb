e3cf213e7115c8bb2ae4a77b52c084ebc0141efd
fukuyumi <kk.ma.yff@gmail.com>
2022-11-24 16:19:26 +0900

    with_optionsにtoken追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,9 +10,9 @@ class OrderAddress
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'input oniy number' }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
