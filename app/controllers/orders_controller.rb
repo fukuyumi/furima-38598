f108b13c8bfc62925e822a442fbfdb4f3399906e
fukuyumi <kk.ma.yff@gmail.com>
2022-11-24 11:02:19 +0900

    クレジットカード購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid? 
+      pay_item
       @order_address.save
       redirect_to root_path(@order_address)
     else
@@ -21,6 +22,15 @@ class OrdersController < ApplicationController
  private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number
-
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :token
+  
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id
@@ -12,6 +12,7 @@ class OrderAddress
     validates :item_id
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
