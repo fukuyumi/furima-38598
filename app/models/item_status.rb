50f1365d9e0844a3707a50abedbb7ce87aa38fc3
fukuyumi <kk.ma.yff@gmail.com>
2022-11-25 14:53:29 +0900

    ActiveHash修正

--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -1,13 +1,12 @@
 class ItemStatus < ActiveHash::Base
   self.data = [
     { id: 1, item_status: '---' },
-    { id: 2, item_status: '新品' },
-    { id: 3, item_status: '未使用' },
-    { id: 4, item_status: '未使用に近い' },
-    { id: 5, item_status: '目立った傷や汚れなし' },
-    { id: 6, item_status: 'やや傷や汚れあり' },
-    { id: 7, item_status: '傷や汚れあり' },
-    { id: 8, item_status: '全体的に状態が悪い' }
+    { id: 2, item_status: '新品・未使用' },
+    { id: 3, item_status: '未使用に近い' },
+    { id: 4, item_status: '目立った傷や汚れなし' },
+    { id: 5, item_status: 'やや傷や汚れあり' },
+    { id: 6, item_status: '傷や汚れあり' },
+    { id: 7, item_status: '全体的に状態が悪い' }
   ]
 
   include ActiveHash::Associations
